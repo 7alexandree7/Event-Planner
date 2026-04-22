2332e38901c1868e73e5361100ed92a11515d720
alexandre <alexandre020602@gmail.com>
2026-04-22 13:35:15 -0300

    feat: add authentication page and dashboard component, update header links

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -8,7 +8,7 @@ const Header = () => {
             <div className='mx-auto  flex h-16 w-full max-w-6xl items-center justify-between px-4'>
                 <Link href="/">Event Planner</Link>
                 <nav className='flex items-center gap-4'>
-                    <Link href="/events" className='px-3 py-2 rounded-md text-sm font-medium hover:bg-(--foreground)/10'>Dashboard</Link>
+                    <Link href="/dashboard" className='px-3 py-2 rounded-md text-sm font-medium hover:bg-(--foreground)/10'>Dashboard</Link>
                     <UserButton size="icon" />
                 </nav>
             </div>
